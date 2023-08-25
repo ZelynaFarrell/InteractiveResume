64b0009c9fc4b951c291d472b2c9bb1afacf64de
Zelyna Farrell <zsillas@yahoo.com>
2023-08-24 17:19:35 -0700

    Initial Commit

---
+++ InteractiveResume/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  InteractiveResume
+//
+//  Created by Zelyna Sillas on 8/24/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ InteractiveResume/InteractiveResumeApp.swift
@@ -0,0 +1,17 @@
+//
+//  InteractiveResumeApp.swift
+//  InteractiveResume
+//
+//  Created by Zelyna Sillas on 8/24/23.
+//
+
+import SwiftUI
+
+@main
+struct InteractiveResumeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
